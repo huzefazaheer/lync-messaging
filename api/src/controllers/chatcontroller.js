d0887ca1d5459a73e0e120f7a1f52afe295ba9fd
Huzefa Zaheer <huzefazaheer1@gmail.com>
2025-08-08 20:53:30 +0500

    Update chat router file structure

---
+++ api/src/controllers/chatcontroller.js
@@ -0,0 +1,36 @@
+const { getAllChats, getChatById, createChat } = require('../models/chatsdb')
+
+async function getAllChatsController(req, res) {
+  try {
+    const chats = await getAllChats()
+    res.json(chats)
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Database Error' })
+  }
+}
+
+async function getChatController(req, res) {
+  try {
+    const chat = await getChatById(req.params.id)
+    res.json(chat)
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Database Error' })
+  }
+}
+
+async function createChatController(req, res) {
+  try {
+    const userIds = JSON.parse(req.body.users)
+    const chat = await createChat(req.user.id, userIds)
+    console.log(chat)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: 'Internal Database Error' })
+  }
+}
+
+module.exports = {
+  getAllChatsController,
+  getChatController,
+  createChatController,
+}
--- api/src/routes/chatrouter.js
+++ api/src/routes/chatrouter.js
@@ -1,36 +1,17 @@
 const { Router } = require('express')
 const { isAuth, isAdmin } = require('../controllers/authcontroller')
-const { getAllChats, getChatById, createChat } = require('../models/chatsdb')
+const {
+  getAllChatsController,
+  getChatController,
+  createChatController,
+} = require('../controllers/chatcontroller')
 
 const chatRouter = Router()
 
-chatRouter.get('/', isAuth, isAdmin, async (req, res) => {
-  try {
-    const chats = await getAllChats()
-    res.json(chats)
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Database Error' })
-  }
-})
+chatRouter.get('/', isAuth, isAdmin, getAllChatsController)
 
-chatRouter.get('/:id', isAuth, async (req, res) => {
-  try {
-    const chat = await getChatById(req.params.id)
-    res.json(chat)
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Database Error' })
-  }
-})
+chatRouter.get('/:id', isAuth, getChatController)
 
-chatRouter.post('/', isAuth, async (req, res) => {
-  try {
-    const userIds = JSON.parse(req.body.users)
-    const chat = await createChat(req.user.id, userIds)
-    console.log(chat)
-  } catch (error) {
-    console.log(error)
-    res.status(500).json({ error: 'Internal Database Error' })
-  }
-})
+chatRouter.post('/', isAuth, createChatController)
 
 module.exports = { chatRouter }
