61bd1cef6f0039a35b2feb1308e323bf6938723f
Huzefa Zaheer <huzefazaheer1@gmail.com>
2025-08-11 20:15:28 +0500

    Updated login and signup styling

--- frontend/src/components/auth/login.jsx
+++ frontend/src/components/auth/login.jsx
@@ -1,20 +1,52 @@
+import { useState } from 'react'
 import styles from './styles.module.css'
 
 export default function Login() {
+  const [showPassword, setShowPassword] = useState(false)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+
+  async function handleLogin() {
+    if (!showPassword && username != '') {
+      setShowPassword(true)
+      return
+    } else setError('Please provide your username')
+  }
+
   return (
-    <div className={styles.authform}>
-      <h1>Login</h1>
-      <label htmlFor="username">Username</label>
-      <input type="text" />
-      <label htmlFor="username">Password</label>
-      <input type="password" />
-      <button type="submit">Login</button>
-      <button type="submit" className={styles.secondary}>
-        Continue as Guest
-      </button>
-      <p>
-        Don't have an account? <span className={styles.link}>Sign Up</span>
-      </p>
-    </div>
+    <>
+      <img className={styles.logo} src="/logo.svg" alt="Lync messaging logo" />
+      <div className={styles.authform}>
+        <h1>Login to Lync</h1>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label htmlFor="username" className={showPassword ? '' : styles.hidden}>
+          Password
+        </label>
+        <input
+          type="password"
+          className={showPassword ? '' : styles.hidden}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <p className={`${styles.error} ${error != '' ? '' : styles.hidden}`}>
+          {error}
+        </p>
+        <button type="submit" onClick={handleLogin}>
+          {showPassword ? 'Login' : 'Continue'}
+        </button>
+        <button type="submit" className={styles.secondary}>
+          Continue as Guest
+        </button>
+        <p>
+          Don't have an account? <span className={styles.link}>Sign Up</span>
+        </p>
+      </div>
+    </>
   )
 }
--- frontend/src/components/auth/signup.jsx
+++ frontend/src/components/auth/signup.jsx
@@ -1,22 +1,63 @@
+import { useState } from 'react'
 import styles from './styles.module.css'
 
 export default function SignUp() {
+  const [showPassword, setShowPassword] = useState(false)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [password2, setPassword2] = useState('')
+  const [error, setError] = useState('')
+
+  async function handleSignup() {
+    if (!showPassword && username != '') {
+      setShowPassword(true)
+      return
+    } else setError('Please provide your username')
+  }
+
   return (
-    <div className={styles.authform}>
-      <h1>Sign Up</h1>
-      <label htmlFor="username">Username</label>
-      <input type="text" />
-      <label htmlFor="username">Password</label>
-      <input type="password" />
-      <label htmlFor="username">Confirm Password</label>
-      <input type="password" />
-      <button type="submit">Sign Up</button>
-      <button type="submit" className={styles.secondary}>
-        Continue as Guest
-      </button>
-      <p>
-        Already have an account? <span className={styles.link}>Login</span>
-      </p>
-    </div>
+    <>
+      {' '}
+      <img className={styles.logo} src="/logo.svg" alt="Lync messaging logo" />
+      <div className={styles.authform}>
+        <h1>Welcome to Lync</h1>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label htmlFor="username" className={showPassword ? '' : styles.hidden}>
+          Password
+        </label>
+        <input
+          type="password"
+          className={showPassword ? '' : styles.hidden}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <label htmlFor="username" className={showPassword ? '' : styles.hidden}>
+          Confirm Password
+        </label>
+        <input
+          type="password"
+          className={showPassword ? '' : styles.hidden}
+          value={password2}
+          onChange={(e) => setPassword2(e.target.value)}
+        />
+        <p className={`${styles.error} ${error != '' ? '' : styles.hidden}`}>
+          {error}
+        </p>
+        <button type="submit" onClick={handleSignup}>
+          {showPassword ? 'Sign Up' : 'Continue'}
+        </button>
+        <button type="submit" className={styles.secondary}>
+          Continue as Guest
+        </button>
+        <p>
+          Already have an account? <span className={styles.link}>Login</span>
+        </p>
+      </div>
+    </>
   )
 }
